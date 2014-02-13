a90979f63a7f34446a5a764f50e05b927ec4ad7a
Brian Byrne <brian.byrne@zonal.co.uk>
2014-02-13 22:10:36 +0000

    page 106 of head first c

--- head3.c
+++ head3.c
@@ -1,22 +1,29 @@
 #include<stdio.h>
 #include<string.h>
 
-int main()
-{
-float latitude;
-float longitude;
-char info[80];
-int started = ;
-puts("data=[");
-while (scanf("%f,%f,%79[^\n]", , , ) == 3) {
-if (started)
-printf(",\n");
-else
-started = ;
-printf("{latitude: %f, longitude: %f, info: '%s'}", , , );
-}
-puts("\n]");
-return 0;
+int main(int argc, char *args[]){
+	float longitude;
+	float latitude;
+	char info[80];
+	
+	int started = 0;
+	
+	puts("data[=");
+	
+	while(scanf("%f,%f,%79[^\n]",longitude, latitude, info)== 3) {
+		if(started) {
+			printf(",\n");
+		}
+		/*
+		else {
+			started = 1;
+		}*/
+		printf("{latitude: %f, longitude: %f}",longitude, latitude);
+		
+	}
+	
+	puts("\n]");
+	return 0;
 }
 
 
