0971bc827d381218bc70778adeefef7578f8b419
Brian Byrne <brian.byrne@zonal.co.uk>
2014-03-13 21:59:45 +0000

    ex17 learn c the hard way. See line 18 - hmmm...

---
+++ ex17.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_DATA 512
+#define MAX_ROWS 100
+
+struct Address {
+    int id;
+    int set;
+    char name[MAX_DATA];
+    char email[MAX_DATA];
+};
+
+struct Database {
+	struct Address rows[MAX_ROWS];
+};
+
+
+
+
+int main(int argc, char *args[]) {
+	
+}
+
+
